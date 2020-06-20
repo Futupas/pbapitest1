9d6a1d3f186d11b72327255318ef5779b6bcc339
Olexandr Panov <futupas@gmail.com>
2020-06-21 01:06:37 +0300

    commit

---
+++ api2.php
@@ -0,0 +1,15 @@
+<?php
+$url = "https://api.privatbank.ua/p24api/rest_fiz";       
+$xml2= file_get_contents('request.xml'); 
+$stream_options = array (
+             'http' => array (
+                   'method' => "POST",
+                   'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                   'content' => $xml2
+                 )
+             );
+$context=stream_context_create($stream_options);
+$response=file_get_contents($url, false, $context);
+$array_data = json_decode(json_encode(simplexml_load_string($response)), true);
+echo $array_data;
+?>
